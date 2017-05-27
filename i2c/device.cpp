e12aa62494067ba535b3d6e358aad87c67d14b38
jamiefmiller <jamie.miller273@gmail.com>
2017-05-26 18:30:24 -0600

    Added support for bulk read/write operations over i2c.

--- i2c/device.cpp
+++ i2c/device.cpp
@@ -5,6 +5,7 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <string>
+#include <vector>
 #include "device.h"
 
 Device::Device(const char i2cBus[], uint8_t addr) {
@@ -17,9 +18,28 @@ uint8_t Device::read(uint8_t address) {
   return i2c_smbus_read_byte_data(i2cBus_, address);
 }
 
+std::vector<uint8_t> Device::readMany(uint8_t address, uint8_t length) {
+  uint8_t* rawRead = new uint8_t[length];
+  i2c_smbus_read_block_data(
+      i2cBus_,
+      address,
+      rawRead);
+}
+
 void Device::write(uint8_t address, uint8_t value) {
   ioctl(i2cBus_, I2C_SLAVE, addr_);
   i2c_smbus_write_byte_data(i2cBus_, address, value);
+}
+
+void Device::writeMany(
+    uint8_t address,
+    std::vector<uint8_t> const &values) {
+  ioctl(i2cBus_, I2C_SLAVE, addr_);
+  i2c_smbus_write_block_data(
+      i2cBus_,
+      address,
+      values.size(),
+      values.data());
 }
 
 int main(void) {
--- i2c/device.h
+++ i2c/device.h
@@ -1,15 +1,18 @@
 #ifndef HARDWARE_I2C_DEVICE_H
 #define HARDWARE_I2C_DEVICE_H
 
-class Device
-{
-public:
-  Device(const char i2cBus[], uint8_t addr);
-  uint8_t read(uint8_t address);
-  void write(uint8_t address, uint8_t value);
-private:
-  int i2cBus_;
-  uint8_t addr_;
+class Device {
+  public:
+    Device(const char i2cBus[], uint8_t addr);
+    uint8_t read(uint8_t address);
+    std::vector<uint8_t> readMany(uint8_t address, uint8_t length);
+    void write(uint8_t address, uint8_t value);
+    void writeMany(
+        uint8_t address,
+        std::vector<uint8_t> const &values);
+  private:
+    int i2cBus_;
+    uint8_t addr_;
 };
 
 #endif
